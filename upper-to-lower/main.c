e57115a1bae5aaf72341652695c7858174e8aa5f
Akshay s nair <94843189+Akshay-s-nair@users.noreply.github.com>
2022-01-25 18:25:05 +0530

    Add files via upload

---
+++ upper-to-lower/main.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   char a,n;
+   printf("Enter the uppercase letter\n");
+   scanf("%c", &n);
+   
+   n=n+32;
+   
+   printf("lowercase is %c", n);
+   
+
+    return 0;
+}
